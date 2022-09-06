30eb12ee024b45298db290c4c6963e6d117f0258
Vyshak-NSC <vyshaknsc02@gmail.com>
2022-09-06 20:04:06 +0530

    Initialised all screens

---
+++ main.py
@@ -0,0 +1,32 @@
+import time, kivy
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
+
+kivy.require('2.1.0')
+
+class Main(GridLayout):
+    pass
+
+
+class ClockScreen(Screen):
+    pass
+
+class StopWatchScreen(Screen):
+    pass
+
+class TimerScreen(Screen):
+    pass
+
+class ScreenManagerClass(ScreenManager):
+    pass
+
+class ClockApp(App):
+    def build(self):
+        self.root = Main()
+        return self.root
+
+if __name__ == '__main__':
+    ClockApp().run()
