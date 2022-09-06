58b96d2b82bd06de7bbe58d920ebbd7878653b59
Vyshak-NSC <vyshaknsc02@gmail.com>
2022-09-06 22:42:17 +0530

    added time, stopwatch functionalities updated UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time, kivy
+import kivy
+from time import strftime
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
@@ -23,7 +24,10 @@ class ScreenManagerClass(ScreenManager):
     pass
 
 class ClockApp(App):
+    started = False
+    timer_count = 0
     prev_scr = 'clock'
+
     def build(self):
         self.main = Main()
         return self.main
@@ -34,5 +38,26 @@ class ClockApp(App):
         self.main.ids['screen_manager'].current = scr_name
         self.main.ids['screen_manager'].transition.direction = direction
         self.prev_scr = scr_name
+    
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0.016)
+
+    def update_time(self, nap):
+        self.main.ids['screen_manager'].get_screen('clock').ids['show_time'].text = strftime('%I:%M:%S %p')
+        if self.started:
+            self.timer_count += nap
+        minutes, seconds = divmod(self.timer_count, 60)
+        
+        self.main.ids['screen_manager'].get_screen('stopwatch').ids['stopwatch'].text = strftime('%02d:%02d:%02d' % (int(minutes), int(seconds), int(seconds*100 %100)))
+
+    def timer_start(self):
+        self.main.ids['screen_manager'].get_screen('stopwatch').ids['start'].text = ('Start' if self.started else 'Stop')
+        self.started = not self.started
+    
+    def timer_reset(self):
+        if self.started:
+            self.started = False
+            self.main.ids['screen_manager'].get_screen('stopwatch').ids['start'].text = 'Start'
+        self.timer_count = 0
 if __name__ == '__main__':
     ClockApp().run()
